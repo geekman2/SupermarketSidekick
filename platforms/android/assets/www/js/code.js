ff27996caf4fe7714188244414fe3016f26ef45f
geekman2 <geekmann2@gmail.com>
2015-03-12 17:51:12 -0700

    Sync fully functional, speed untested

--- platforms/android/assets/www/js/code.js
+++ platforms/android/assets/www/js/code.js
@@ -41,46 +41,41 @@ function logout(){
     window.location.replace("index.html");
 }
 
-function syncSidekick(){
+function syncCallback(response){
+
     //The sync function checks the location that the user has currently set and
     //then synchronises the local database with the cloud database that match that
     //location
     var store = "la-crescenta-vons"; //TODO add location setting
     var dbname = store+"-DB";
     var db = window.sqlitePlugin.openDatabase({name: dbname, location: 1});
-    //var db = sqlitePlugin.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-
-    var docs = httpGet("http://54.183.22.36/big/_design/items/_view/ItemsByDate");
-    //var docs = readTextFile("file:///C:/Users/Geekman2/Documents/Github/Phonegap/SupermarketSidekick/www/response.txt");
-
-    var docsj= JSON.parse(docs);
+    dropDB(db);
+    initializeDB(db);
+    var docsj= JSON.parse(response);
 
     for(var i = 0; i < docsj.rows.length; i++) {
         var obj = docsj.rows[i];
         var item = obj.value.item;
-        var aisle = obj.value.item;
+        var aisle = obj.value.aisle;
+        var itemString = JSON.stringify(item);
+        var aisleString = JSON.stringify(aisle);
+        insertDB(db,itemString,aisleString);
         //TODO add local database support
         //TODO add item value pairs to local database
         //TODO sync user points
     }
+    queryDB(db);
 }
 
-function search(){
-    //TODO search local database and return results
-    }
-
-function additem(){
-    //TODO allow users to PUT new items in
-    }
-function itemvalidate(){
-    //TODO validate items to make sure they don't have illegal characters, etc
-    }
-function httpGet(){
-    var repo = $.get('http://www.google.com',function(data){
-        console.log(data);
-        alert("Passed Data")
-        return data;
-    })
+function syncSidekick(url){
+    $.ajax({
+     async: false,
+     type: 'GET',
+     url: url,
+     success: function(response) {
+          syncCallback(response);
+     }
+});
 }
 
 function readTextFile(file)
@@ -92,3 +87,66 @@ function readTextFile(file)
     return allText;
 
 }
+// Query the success callback
+//
+function querySuccess(tx,results) {
+    var i;
+    //Iterate through the results
+    for (i = 0; i < results.rows.length; i++) {
+        //Get the current row
+        var row = results.rows.item(i);
+        console.log(row);
+    }
+}
+
+// Transaction error callback
+//
+function errorCB(err) {
+    console.log("Error processing SQL: "+err.message+' '+err.stack);
+}
+
+// Transaction success callback
+//
+function successCB() {
+    console.log("Database Initialized");
+}
+
+function dropDB(db){
+    db.transaction(function(tx){
+        tx.executeSql('DROP TABLE IF EXISTS DB');
+        console.log("Dropped");
+        }
+    );
+}
+
+function initializeDB(db){
+    db.transaction(function(tx){
+
+        tx.executeSql('CREATE TABLE IF NOT EXISTS DB (id INTEGER PRIMARY KEY AUTOINCREMENT , item TEXT, aisle TEXT)',[],successCB,errorCB);
+        }
+    );
+}
+
+function insertDB(db,itemString,aisleString){
+    db.transaction(function(tx){
+        tx.executeSql('INSERT INTO DB (item,aisle) VALUES (?,?)',[itemString,aisleString,],successCB,errorCB);
+        console.log("Added");
+    });
+}
+
+function queryDB(db){
+    db.transaction(function(tx) {
+            tx.executeSql("SELECT * FROM DB", [], querySuccess);
+        });
+    }
+
+function search(){
+    //TODO search local database and return results
+    }
+
+function additem(){
+    //TODO allow users to PUT new items in
+    }
+function itemvalidate(){
+    //TODO validate items to make sure they don't have illegal characters, etc
+    }
